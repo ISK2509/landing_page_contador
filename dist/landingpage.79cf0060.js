1cfea224e48081bf0cdf28b2baf3879562fc3fe5
ivan <ivan@teste.com>
2025-05-30 18:10:02 -0300

    fix: ajuste versão sharp

---
+++ dist/landingpage.79cf0060.js
@@ -0,0 +1,2 @@
+AOS.init();let dataDoEvento=new Date("Dec 12, 2025 19:00:00"),timeStampDoEvento=dataDoEvento.getTime(),contaAsHoras=setInterval(function(){let e=timeStampDoEvento-new Date().getTime(),t=Math.floor(e/864e5),o=Math.floor(e%864e5/36e5),n=Math.floor(e%36e5/6e4),a=Math.floor(e%6e4/1e3);document.getElementById("contador").innerHTML=`${t}d ${o}h ${n}m ${a}s`,e<0&&(clearInterval(contaAsHoras),document.getElementById("contador").innerHTML="Evento Expirado")},1e3);
+//# sourceMappingURL=landingpage.79cf0060.js.map
